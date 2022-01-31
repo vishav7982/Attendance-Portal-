77d9521eba42b1e694da2a1876ca80dec4fd9a1a
Vishav Singh <vishavsingh7982@gmail.com>
2022-01-31 17:40:49 +0530

    Added body and styling to two components

--- src/app/emp-my-attendance/emp-my-attendance.component.ts
+++ src/app/emp-my-attendance/emp-my-attendance.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import pkg from '../../assets/data/json/attendanceRecord.json';
+import { ColDef } from 'ag-grid-community';
+import {Chart} from 'chart.js';
 @Component({
   selector: 'app-emp-my-attendance',
   templateUrl: './emp-my-attendance.component.html',
@@ -7,8 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpMyAttendanceComponent implements OnInit {
 
+  //Ag-grid
+  columnDefs: ColDef[] = [
+    { headerName : 'Day' , field: 'Day' ,cellClass: "grid-cell-centered"},
+    {  headerName : 'In-time' ,field: 'In-time',cellClass: "grid-cell-centered"},
+    {headerName: 'Out-time' ,  field: 'Out-time',cellClass: "grid-cell-centered"},
+    {headerName:"Working-hours" ,field : 'working-hours',cellClass: "grid-cell-centered"}
+    
+];
+  data = pkg.data; 
+  AttendanceData: Array<{ [key: string]: string | number | object }> = this.data;
+  rowData = this.AttendanceData;
+  
+  ///bar chart declarations
+  chartData = [
+    {
+      data: [9,9,9.25,9,10.5,0,0],
+      label: 'In-time'
+    },
+    {
+      data: [5.5, 6, 6.25, 6.5,8.5 ,0,0],
+      label: 'Out-time'
+    }
+  ];
+  chartLabels = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thrusday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  chartOptions = {
+    responsive: true
+  };
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
